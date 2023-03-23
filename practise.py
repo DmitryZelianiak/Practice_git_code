0eb54ac8ddaeac1c3fa9516cfac3d6866a23a440
Dzmitry Zelianiak <dim4coin@gmail.com>
2023-03-23 17:25:04 +0100

    initial commmit

---
+++ practise.py
@@ -0,0 +1,28 @@
+n = [1,4,6]
+x = ['koko', 'bad', 'don']
+z = [33, 44, 55]
+
+# for ind, value in enumerate(zip(n, x,z)):
+#     print(ind, value)
+    
+
+
+new_dic = {val: number for val, 
+          number in zip(val, number)}
+print(new_dic)
+
+    
+
+
+# n1 = [21,33,32,4]
+# n2 = [1, 1, 1, 1, 1, 1]
+# kv = list(map(lambda x, y: x + y, n1, n2))
+# print(kv)
+# a = 5
+# b = 7
+# c = 4
+# rev_upper = lambda x: x.upper() [::-1]
+# print(rev_upper(st))
+
+# maximum = lambda x, y, z: max(x,y,z)
+# print(maximum(a,b,c))
